4391f145ef7d2d59973e32afa494d9c16211abe1
Simon Kocher <simonk2004@t-online.de>
2018-04-16 19:40:42 +0200

    Expanded logging (slightly)

--- src/log.cpp
+++ src/log.cpp
@@ -5,11 +5,21 @@
 
 static loglevel_t G_LOGLEVEL = LINFO;
 
+static const char *lltos(loglevel_t ll) {
+	switch (ll) {
+		case LINFO: return "INFO";
+		case LWARN: return "WARN";
+		case LERROR: return "ERROR";
+		default: return "<Unkown>";
+	}
+}
+
 void log(loglevel_t lvl, const char *msg, ...) {
 	va_list args;
 	va_start(args, msg);
 
 	if (lvl >= G_LOGLEVEL) {
+		printf("[%s] ", lltos(lvl));
 		vprintf(msg, args);
 	}
 
